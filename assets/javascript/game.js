b2f5750507cfb02e3ff21d9a67d0533c702dce05
susan2k83 <susan2k83@yahoo.com>
2017-08-17 15:22:37 -0700

    yet more code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,45 @@
+//Computer chooses a random letter.
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+var wins = 0;
+var losses = 0;
+var maxGuessNumber = 9;
+document.getElementById("guesses-left-number").textContent = maxGuessNumber;
+console.log("computer guess was:", computerGuess);
+//The user types in a letter.
+document.onkeyup = function(event) {
+    var userGuess = event.key;
+    console.log("user guess was:", userGuess);
+    document.getElementById("user-guess").textContent = userGuess;
 
-    //Computer chooses a random letter.
-    var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-	var wins= 0;
-	var losses = 0;
-	var maxGuesses = 9;
-
-		console.log("computer guess was:", computerGuess);
-	//The user types in a letter.
-	 document.onkeyup = function(event) {
-	 	var userGuess = event.key;
-	 	console.log("user guess was:", userGuess);
-	 	document.getElementById("user-guess").textContent = userGuess;
-
-	 	// for (userGuess===computerGuess; )
+    maxGuessNumber--;
+   	document.getElementById("guesses-left-number").textContent = maxGuessNumber;
+    // for (userGuess===computerGuess; )
 
 
-	//The user guess is displayed on the screen:   	"Your Gueses so far".
-	    if (userGuess===computerGuess){
-            console.log(userGuess + "You are correct ");
+    //The user guess is displayed on the screen:   	"Your Gueses so far".
+    if (userGuess === computerGuess) {
+        console.log("You are correct!");
+        wins++; //wins = wins + 1;
+        document.getElementById("win-number").textContent = wins;
+        //The Wins go up by one with each win.
 
-             document.getElementById("win-number").textContent = wins;
+    } else {
+        console.log(userGuess + " is not correct");
+    }
 
-	    } else {
-	    	console.log(userGuess + " is not correct");
-	    }
-	//The user gets 9 guesses.
-         for (wins = 0; wins < 9; wins++ ) {
-             console.log(wins);
-         }
-	//After each guess the 'Guesses Left' goes down by one.
+     //The user gets 9 guesses.
+    if (maxGuessNumber === 0) {
+        console.log("You have reached the maximum number of guesses");
+        alert("Game over");
 
-	//The game resets after 9 guesses and the computer chooses a new number or, when the user gueses the right number before the 9 guesses.
+	    //The Loses go up by one with each loss. 
+    }
+   
 
-	//The Wins go up by one with each win.
+    //After each guess the 'Guesses Left' goes down by one.
 
-	//The Loses go up by one with each loss. 
-	    
+    //The game resets after 9 guesses and the computer chooses a new number or, when the user gueses the right number before the 9 guesses.
+
+
 };
